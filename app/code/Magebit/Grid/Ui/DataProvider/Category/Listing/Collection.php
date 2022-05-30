1d1c8de9df6411c019485e72d6d8531cb0df0bbb
Rihards Edels <rihards.edels@magebit.com>
2022-05-30 17:09:03 +0300

    Learning Tab

--- app/code/Magebit/Grid/Ui/DataProvider/Category/Listing/Collection.php
+++ app/code/Magebit/Grid/Ui/DataProvider/Category/Listing/Collection.php
@@ -9,7 +9,7 @@ class Collection extends SearchResult
       protected function _initSelect()
       {
           $this->addFilterToMap('entity_id', 'main_table.entity_id');
-          $this->addFilterToMap('name', 'devgridname.value');
+          $this->addFilterToMap('name', 'learninggridname.value');
           parent::_initSelect();
       }
 }
