f7d0cb1565c905aa60e831697f7f863c50c2542a
Rihards Edels <rihards.edels@magebit.com>
2022-06-01 14:17:28 +0300

    FAQ menu working

---
+++ app/code/Magebit/Faq/Controller/Adminhtml/Question/Index.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Magebit\Faq\Controller\Adminhtml\Question;
+
+class Index extends \Magento\Backend\App\Action
+
+{
+
+    protected $resultPageFactory;
+
+    public function __construct(
+
+        \Magento\Backend\App\Action\Context $context,
+
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+
+    ) {
+
+        parent::__construct($context);
+
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    public function execute()
+
+    {
+
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('Magento_Backend::content');
+        $resultPage->getConfig()->getTitle()->prepend(__('Frequently Asked Questions'));
+
+        return $resultPage;
+    }
+}
--- app/code/Magebit/Grid/Model/ResourceModel/Category.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Magebit\Grid\Model\ResourceModel;
-
-
-class Category extends \Magento\Catalog\Model\ResourceModel\Category
-{
-}
